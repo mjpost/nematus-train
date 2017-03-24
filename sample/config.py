b2a9e33a91ba464fe8be66f66712647d4558929f
Matt Post <post@cs.jhu.edu>
2017-03-24 12:59:37 -0400

    now builds train-iter, passes params in, more params

--- sample/config.py
+++ sample/config.py
@@ -1,31 +1,26 @@
 import numpy
 import os
 import sys
+import argparse
 
-VOCAB_SIZE = 90000
 
-# Assumes the presence of a file "params.txt"
-for line in open('params.txt'):
-    if line.startswith('#'):
-        continue
-
-    if '=' in line:
-        key, value = line.rstrip().split('=')
-        key = key.strip()
-        value = value.strip()
-
-        if key == 'SRC':
-            SRC = value
-        elif key == 'TRG':
-            TRG = value
-        elif key == 'VOCAB_SIZE':
-            VOCAB_SIZE = int(value)
-
-DATA_DIR = "data/"
+parser = argparse.ArgumentParser(description='Run Nematus training.')
+parser.add_argument('-vocab-size', type=int, default=50000, help='Size of the vocabulary')
+parser.add_argument('-source', type=str, required=True, help='Source language')
+parser.add_argument('-target', type=str, required=True, help='Source language')
+parser.add_argument('-runtime', type=int, default=0, help='How long to run for (in seconds)')
+parser.add_argument('-data-dir', type=str, default='data', help='Where to find the data')
 
 from nematus.nmt import train
 
 if __name__ == '__main__':
+    args = parser.parse_args()
+
+    VOCAB_SIZE = args.vocab_size
+    SRC = args.source
+    TRG = args.target
+    DATA_DIR = args.data_dir
+
     validerr = train(saveto='model/model.npz',
                     reload_=True,
                     dim_word=500,
@@ -42,10 +37,11 @@ if __name__ == '__main__':
                     datasets=[DATA_DIR + '/train.bpe.' + SRC, DATA_DIR + '/train.bpe.' + TRG],
                     valid_datasets=[DATA_DIR + '/validate.bpe.' + SRC, DATA_DIR + '/validate.bpe.' + TRG],
                     dictionaries=[DATA_DIR + '/train.bpe.' + SRC + '.json',DATA_DIR + '/train.bpe.' + TRG + '.json'],
-                    validFreq=1000,
+                    validFreq=10000,
                     dispFreq=1000,
-                    saveFreq=1000,
+                    saveFreq=10000,
                     sampleFreq=10000,
+                    runTime=args.runtime,
                     use_dropout=False,
                     dropout_embedding=0.2, # dropout for input embeddings (0: no dropout)
                     dropout_hidden=0.2, # dropout for hidden layers (0: no dropout)
--- sample/train.sh
+++ sample/train.sh
@@ -5,12 +5,44 @@
 
 . params.txt
 
+maxiters=9999
+if [[ ! -z $1 ]]; then
+    maxiters=$1
+fi
+
+echo "Running for at most $maxiters iterations..."
+
+# Create the qsub file
+cat > train-$SRC-$TRG.sh <<EOF
+#!/bin/bash
+
+. ./params.txt
+
 # Load the GPU-specific commands if necessary
 if [[ $device = "gpu" ]]; then
-    . gpu.sh
+    . ./gpu.sh
 fi
 
 devno=$($TRAIN/get-gpus.sh)
 echo "Using device gpu$devno"
 
-THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=$device$devno,on_unused_input=warn python config.py
+THEANO_FLAGS=device=$device$devno python config.py -runtime $RUNTIME -vocab-size $VOCAB_SIZE -source $SRC -target $TRG
+EOF
+
+iter=1
+[[ ! -d logs ]] && mkdir logs
+while true; do
+    echo "ITERATION $iter / $maxiters"
+
+    # Run an iteration of training lasting at most two hours. Make sure that
+    # saveFreq is set low enough in config.py to save within that amount of time
+    # (1000 should be sufficient though it is relatively often)
+    qsub -sync y -cwd -S /bin/bash -V -l gpu=1,h_rt=$RUNTIME -j y -o logs/ ./train-$SRC-$TRG.sh
+
+    if [[ $iter -ge $maxiters ]]; then
+        echo "Quitting."
+        break
+    fi
+
+    let iter=iter+1
+done
