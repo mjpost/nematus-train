c1d7abd60a05f272d851f1f603e8279dc13464c4
Matt Post <post@cs.jhu.edu>
2017-05-04 12:33:57 -0400

    now processes all the factors you give it

--- preprocess/conll_to_factors.py
+++ preprocess/conll_to_factors.py
@@ -5,16 +5,21 @@
 
 # take conll file, and bpe-segmented text, and produce factored output
 
+import codecs
 import sys
 import re
 
 from collections import namedtuple
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+sys.stdin = codecs.getreader('utf-8')(sys.stdin)
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+sys.stdout.encoding = 'utf-8'
 
 Word = namedtuple(
     'Word',
-    ['pos', 'word', 'lemma', 'tag', 'morph', 'head', 'func', 'proj_head', 'proj_func'])
-
+    ['number', 'word', 'factors'])
 
 def escape_special_chars(line):
     line = line.replace('\'', '&apos;')  # xml
@@ -30,73 +35,44 @@ def read_sentences(fobj):
 
     for line in fobj:
 
+        line = line.replace('|', '-PIPE-')
+
         if line == "\n":
             yield sentence
             sentence = []
             continue
 
-        try:
-            (
-                pos,
-                word,
-                lemma,
-                tag,
-                tag2,
-                morph,
-                head,
-                func,
-                proj_head,
-                proj_func,
-            ) = line.split()
-        except ValueError:  # Word may be unicode whitespace.
-            (
-                pos,
-                word,
-                lemma,
-                tag,
-                tag2,
-                morph,
-                head,
-                func,
-                proj_head,
-                proj_func,
-            ) = re.split(' *\t*', line.strip())
+        pos,word,rest = line.split('\t', 2)
+        tokens = rest.split()
 
         word = escape_special_chars(word)
-        lemma = escape_special_chars(lemma)
-        morph = morph.replace('|',',')
-
-        if proj_head == '_':
-            proj_head = head
-            proj_func = func
 
         sentence.append(
-            Word(
-                int(pos), word, lemma, tag2, morph, int(head), func, int(proj_head),
-                proj_func))
+            Word(int(pos), word, tokens))
 
 
 def get_factors(sentence, idx):
-
     word = sentence[idx]
-
-    factors = [word.lemma, word.tag, word.func]
-
-    return factors
+    return word.factors
 
 #text file that has been preprocessed and split with BPE
-bpe_file = open(sys.argv[1])
+bpe_file = codecs.open(sys.argv[1], 'r', 'utf-8')
 
 #conll file with annotation of original corpus; mapping is done by index, so number of sentences and words (before BPE) must match
 conll_file = open(sys.argv[2])
 conll_sentences = read_sentences(conll_file)
 
-for line in bpe_file:
+for lineno, line in enumerate(bpe_file, 1):
+  line = line.replace(u'\u200B', '')
   state = "O"
   i = 0
   sentence = conll_sentences.next()
   for word in line.split():
-    factors = get_factors(sentence, i)
+    try:
+        factors = get_factors(sentence, i)
+    except IndexError:
+        sys.stderr.write('BAD LINE {}'.format(lineno))
+        sys.exit(1)
     if word.endswith('@@'):
       if state == "O" or state == "E":
         state = "B"
