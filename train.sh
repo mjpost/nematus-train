066d91f1643a16b1960d09c1bc7fa289bfb9e7f5
Matt Post <post@cs.jhu.edu>
2017-05-17 13:34:28 -0400

    rewrote train.sh to use nmt.py directly

--- train.sh
+++ train.sh
@@ -26,13 +26,58 @@ fi
 devno=\$(\$TRAIN/get-gpus.sh)
 echo "Using device gpu\$devno"
 
+# Adjust path to training data
+BPE_OR_FACTOR=bpe
+if [[ \$FACTORS -gt 1 ]]; then
+  BPE_OR_FACTOR=factors
+fi
+
+# Build dictionary list for factored or unfactored models
+DICTS="train.bpe.\$SRC.json"
+if [[ \$FACTORS -gt 1 ]]; then
+  for num in \$(seq 1 \$FACTORS); do
+    DICTS+=" train.factors.\$num.\$SRC.json"
+  done
+fi
+DICTS+=" train.bpe.\$TRG.json"
+
+# Set dimensions for factored training
+if [[ \$FACTORS -gt 1 ]]; then
+  # DIMS should have been set
+  if [[ -z \$DIMS ]]; then
+    echo "You need to specify \$DIMS for factored training"
+    exit
+  fi
+  DIMS="--dim_per_factor \$DIMS"
+fi
+
 [[ ! -d model ]] && mkdir model
-THEANO_FLAGS=device=\$device\$devno python \$TRAIN/config.py -runtime \$RUNTIME -vocab-size \$VOCAB_SIZE -source \$SRC -target \$TRG -validate-script \$TRAIN/validate-qsub.sh -batch-size \$BATCHSIZE -factors \$FACTORS \$DIMS
+THEANO_FLAGS=device=\$device\$devno python \$nematus/nematus/nmt.py \\
+  --reload \\
+  --saveFreq 10000 \\
+  --dim_word 500 \\
+  --dim 1000 \\
+  --model model/model.npz \\
+  --datasets data/train.\$BPE_OR_FACTOR.\$SRC data/train.bpe.\$TRG \
+  --dictionaries \$DICTS \\
+  --n_words \$VOCAB_SIZE \\
+  --n_words_src \$VOCAB_SIZE \\
+  --runtime \$RUNTIME \\
+  --maxlen 50 \\
+  --optimizer adam \\
+  --batch_size \$BATCHSIZE \\
+  --valid_datasets data/validate.\$BPE_OR_FACTOR.\$SRC data/validate.bpe.\$TRG \\
+  --validFreq 10000 \\
+  --patience 10 \\
+  --external_validation_script \$TRAIN/validate-qsub.sh \\
+  --dispFreq 1000 \\
+  --sampleFreq 10000 \\
+  --factors \$FACTORS \\
+  \$DIMS
 EOF
 
-iter=1
 [[ ! -d logs ]] && mkdir logs
-while true; do
+for iter in $(seq 1 $maxiters); do
     echo "ITERATION $iter / $maxiters"
 
     # Run an iteration of training lasting at most two hours. Make sure that
